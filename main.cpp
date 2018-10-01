0f2c1fc283b5d2ffa0721688bdae921ae5bd87a2
Jeremy D Rhodes <jdrhodes@ecc-jaguar.csuchico.edu>
2018-10-01 10:42:25 -0700

    Shouts

--- main.cpp
+++ main.cpp
@@ -10,7 +10,17 @@ int main()
 
   cout<<"What do you say?\n";
   getline(cin,said);
-
+  for(int index = 0; index < said.length();index++)
+  {        
+          char converted = said[index];
+          if(said[index] > '`' && said[index] < '{')
+          {
+                  converted = said[index] - ('a' - 'A');
+          }
+          said[index] = converted; 
+   
+          cout<<said[index]<<" "<<converted<<endl;
+  }
   cout<<"As a shout, you said:\n"<<said<<endl;
 
   return 0;
